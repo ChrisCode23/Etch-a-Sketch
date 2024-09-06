47b25892736b4132942882808a370a0c735c6b89
ChrisCode23 <178373270+ChrisCode23@users.noreply.github.com>
2024-09-06 17:16:15 +0200

    Create grid with flexbox
    Created row div inside container and cloned it
    16 times, then cloned the square 16 times inside
    each row. With flexbox, adjusted the
    position and gap between each square and each row
    and the final result is a 16 x 16 grid of square
    divs with flexbox

--- javascript.js
+++ javascript.js
@@ -3,8 +3,22 @@ const container = document.querySelector(".container");
 const square = document.createElement("div");
 square.classList.add("square");
 
+const row = document.createElement("div");
+row.classList.add("row");
 
-
-for (let i = 0; i < 16; i++) {
-    container.appendChild(square.cloneNode(true));;
+for (let i = 0; i < 16; i++){
+    container.appendChild(row.cloneNode(true));
 }
+
+const rows = document.querySelectorAll(".row");
+
+rows.forEach((row) => {
+    for (let i = 0; i < 16; i++){
+        row.appendChild(square.cloneNode(true));
+    }
+});
+
+
+//There are 16 rows, each one has the square which is cloned by 16. Square is child of row. Row is child of container
+
+
