cc9c24c5e48f9c7c08b40d633dfd2d21c4877d74
ChrisCode23 <178373270+ChrisCode23@users.noreply.github.com>
2024-09-07 11:52:51 +0200

    Fix square size
    Included "box sizing = border box" to ensure that
    each square's size is 16 x 16 px

--- javascript.js
+++ javascript.js
@@ -6,12 +6,15 @@ square.classList.add("square");
 const row = document.createElement("div");
 row.classList.add("row");
 
+// Clones row 16 times inside container
 for (let i = 0; i < 16; i++){
     container.appendChild(row.cloneNode(true));
 }
 
+// Selects all rows
 const rows = document.querySelectorAll(".row");
 
+// Each row will append 16 cloned squares
 rows.forEach((row) => {
     for (let i = 0; i < 16; i++){
         row.appendChild(square.cloneNode(true));
